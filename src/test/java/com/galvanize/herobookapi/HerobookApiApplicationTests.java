103f66c7e74a49ad0c0579fcd9e8b39d02b2bfaa
AsiyatK <aliciabostan@gmail.com>
2021-01-12 13:00:39 -0500

    initial commit

---
+++ src/test/java/com/galvanize/herobookapi/HerobookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.herobookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerobookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
